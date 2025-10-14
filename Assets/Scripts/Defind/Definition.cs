6364e76c57787617e8461308c28ce10d114b81d6
NhacPhi <chicongchibac@gmail.com>
2025-10-14 14:07:52 +0700

    + Add file meta missing.

---
+++ Assets/Scripts/Defind/Definition.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+
+
+public static class Definition
+{
+    public readonly static List<string> SettingsLanguage = new List<string> { "ENGLISH", "VIETNAMESE" };
+    public readonly static List<string> SettingsFPS = new List<string> { "30", "60" };
+}
