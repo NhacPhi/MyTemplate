39ba2ff75d9ace070698b2a5cd0eb40f0f020c89
NhacPhi <chicongchibac@gmail.com>
2025-11-20 23:24:57 +0700

    + Add game config for game narrative.

--- Assets/Editor/CsvToTextConverter.cs
+++ Assets/Editor/CsvToTextConverter.cs
@@ -117,31 +117,31 @@ public static class CsvToTextConverter
                 switch (sheetName)
                 {
                     case "Food":
-                        ExportSheet<FoodConfig>(sheet, sheetName);
+                        ExportGameConfigSheet<FoodConfig>(sheet, sheetName);
                         break;
                     case "Weapon":
-                        ExportSheet<WeaponConfig>(sheet, sheetName);
+                        ExportGameConfigSheet<WeaponConfig>(sheet, sheetName);
                         break;
                     case "GemStone":
-                        ExportSheet<GemStoneConfig>(sheet, sheetName);
+                        ExportGameConfigSheet<GemStoneConfig>(sheet, sheetName);
                         break;
                     case "BaseArmor":
-                        ExportSheet<BaseArmorConfig>(sheet, sheetName);
+                        ExportGameConfigSheet<BaseArmorConfig>(sheet, sheetName);
                         break;
                     case "Shard":
-                        ExportSheet<ShardConfig>(sheet, sheetName);
+                        ExportGameConfigSheet<ShardConfig>(sheet, sheetName);
                         break;
                     case "Character":
-                        ExportSheet<CharacterConfig>(sheet, sheetName);
+                        ExportGameConfigSheet<CharacterConfig>(sheet, sheetName);
                         break;
                     case "CharacterStat":
-                        ExportSheet<CharacterStatConfig>(sheet, sheetName);
+                        ExportGameConfigSheet<CharacterStatConfig>(sheet, sheetName);
                         break;
                     case "CharacterUpgrade":
-                        ExportSheet<CharacterUpgradeConfig>(sheet, sheetName);
+                        ExportGameConfigSheet<CharacterUpgradeConfig>(sheet, sheetName);
                         break;
                     case "Exp":
-                        ExportSheet<ExpConfig>(sheet, sheetName);
+                        ExportGameConfigSheet<ExpConfig>(sheet, sheetName);
                         break;
                     default: break;
                 }
@@ -152,7 +152,41 @@ public static class CsvToTextConverter
         Debug.Log("Convert've done for all sheet!");
     }
 
-    private static void ExportSheet<T>(ISheet sheet, string sheetName)
+    [MenuItem("ConvertCSV/Narrative")]
+    public static void ConvertNarrative()
+    {
+        string excelPath = "Assets/Data/GameNarrative.xlsx";
+
+        if (!File.Exists(excelPath))
+        {
+            Debug.LogError("Don't find the file path: " + excelPath);
+            return;
+        }
+
+        using (FileStream stream = new FileStream(excelPath, FileMode.Open, FileAccess.Read))
+        {
+            IWorkbook workbook = new XSSFWorkbook(stream);
+
+            for (int i = 0; i < workbook.NumberOfSheets; i++)
+            {
+                ISheet sheet = workbook.GetSheetAt(i);
+                string sheetName = sheet.SheetName;
+                Debug.Log($"Đang xử lý sheet: {sheetName}");
+
+                switch (sheetName)
+                {
+                    case "Actor":
+                        ExportNarrativeSheet<ActorConfig>(sheet, sheetName);
+                        break;
+                    default: break;
+                }
+            }
+        }
+
+        AssetDatabase.Refresh();
+        Debug.Log("Convert've done for all sheet!");
+    }
+    private static void ExportGameConfigSheet<T>(ISheet sheet, string sheetName)
     {
         List<T> list = new List<T>();
 
@@ -254,7 +288,91 @@ public static class CsvToTextConverter
         Json.SaveJson(list, filePath);
         Debug.Log($"Xuất {sheetName}.json ({list.Count} dòng)");
     }
+    private static void ExportNarrativeSheet<T>(ISheet sheet, string sheetName)
+    {
+        List<T> list = new List<T>();
 
+        // Lấy header
+        IRow header = sheet.GetRow(0);
+        int colCount = header.LastCellNum;
+
+
+        bool hasValue;
+        // Đọc từng dòng
+        for (int r = 1; r <= sheet.LastRowNum; r++)
+        {
+            hasValue = true;
+            IRow row = sheet.GetRow(r);
+            if (row == null) continue;
+
+            T obj = System.Activator.CreateInstance<T>();
+            var properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            Dictionary<string, PropertyInfo> propMap = properties.ToDictionary(p => p.Name.ToLower(), p => p);
+            var columnMap = new Dictionary<int, PropertyInfo>();
+
+            for (int c = 0; c < colCount && c < properties.Length; c++)
+            {
+                string headerName = Normalize(header.GetCell(c)?.StringCellValue);
+                if (string.IsNullOrEmpty(headerName)) continue;
+
+                propMap.TryGetValue(headerName, out var prop);
+                if (prop == null) continue;
+
+                var field = prop;
+                ICell cell = row.GetCell(c);
+                if (cell == null) continue;
+
+                try
+                {
+                    object value = null;
+                    switch (cell.CellType)
+                    {
+                        case CellType.String:
+                            {
+                                value = cell.StringCellValue;
+                            }
+                            break;
+                        case CellType.Numeric:
+                            if (field.PropertyType == typeof(int))
+                                value = (int)cell.NumericCellValue;
+                            else if (field.PropertyType == typeof(float))
+                                value = (float)cell.NumericCellValue;
+                            else
+                                value = cell.NumericCellValue.ToString();
+                            break;
+                        case CellType.Boolean:
+                            value = cell.BooleanCellValue;
+                            break;
+                        default:
+                            value = cell.ToString();
+                            break;
+                    }
+
+                    if (value != null)
+                        field.SetValue(obj, System.Convert.ChangeType(value, field.PropertyType));
+                    else
+                    {
+                        hasValue = false;
+                    }
+                }
+                catch (System.Exception ex)
+                {
+                    Debug.LogError($"Lỗi đọc cột {c} hàng {r} trong sheet {sheetName}: {ex.Message}");
+                }
+
+            }
+            if (hasValue)
+            {
+                list.Add(obj);
+            }
+        }
+
+        string folder = "Assets/Data/Narrative/";
+        string filePath = Path.Combine(folder, $"{sheetName}.json");
+        Json.SaveJson(list, filePath);
+        Debug.Log($"Xuất {sheetName}.json ({list.Count} dòng)");
+    }
     private static string Normalize(string name)
     {
         return name?.Trim().Replace(" ", "").ToLower() ?? "";
---
+++ Assets/Scripts/Core/GameConfig/Narrative/ActorConfig.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class ActorConfig
+{
+    private string id;
+    private string name;
+
+    public string ID { get { return id; } set { id = value; } }
+    public string Name { get { return name; } set { name = value; } }
+}
